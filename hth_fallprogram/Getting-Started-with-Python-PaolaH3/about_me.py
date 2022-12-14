cb18fa4ff19973c348c3501fd90529dc36fbb305
PaolaHernandezGodoy <115198123+PaolaHernandezGodoy@users.noreply.github.com>
2022-12-13 17:09:47 -0800

    Add files via upload

---
+++ hth_fallprogram/Getting-Started-with-Python-PaolaH3/about_me.py
@@ -0,0 +1,16 @@
+
+name = "Paola Hernandez" 
+print ("I am "+ name)
+nickname = "Paolita"
+print('My nickname is '+ nickname)
+age = 16
+value = input("Have you used python before?\n")
+if value == 'yes':
+  print(True)
+else:
+  print(False)
+hobbies = ['soccer', 'violin', 'running', 'reading', 'netflix']
+my_dict = {'The nightmare before christmas', 'Tacos','violin'}
+print('My dictionary', my_dict)
+
+all_vars = dict(vars())
